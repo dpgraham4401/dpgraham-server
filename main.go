814ee9f8a91e9b06bea5e2a11b364abfbe36eb36
David Graham <dpgraham4401@gmail.com>
2022-08-29 13:11:40 -0400

    prototype get all blog handler, fix db case

--- main.go
+++ main.go
@@ -11,8 +11,8 @@ import (
 	"strconv"
 )
 
-// Db holds database connection
-var Db *sql.DB
+// db holds database connection
+var db *sql.DB
 
 func init() {
 	var err error
@@ -23,7 +23,7 @@ func init() {
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_NAME"))
-	Db, err = sql.Open("postgres", pgConn)
+	db, err = sql.Open("postgres", pgConn)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -42,23 +42,49 @@ type Article struct {
 
 func main() {
 	router := gin.Default()
-	router.GET("/blog/:id", getBlogs)
+	router.GET("/blog", getAllBlogs)
+	router.GET("/blog/:id", getBlog)
 	err := router.Run(":8080")
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func getBlogs(c *gin.Context) {
+func getBlog(c *gin.Context) {
 	id := c.Param("id")
 	idInt, _ := strconv.Atoi(id)
 	myArticle, _ := queryBlog(idInt)
 	c.IndentedJSON(http.StatusOK, myArticle)
 }
 
+func getAllBlogs(c *gin.Context) {
+	myArticle, _ := queryAllBlog()
+	c.IndentedJSON(http.StatusOK, myArticle)
+}
+
+func queryAllBlog() ([]Article, error) {
+	rows, err := db.Query("SELECT * FROM article WHERE published = true")
+	if err != nil {
+		return nil, err
+	}
+	var blogs []Article
+	for rows.Next() {
+		var blog Article
+		if err := rows.Scan(&blog.Id, &blog.Title, &blog.LastUpdate, &blog.Published,
+			&blog.ArticleType, &blog.Content, &blog.CreatedDate); err != nil {
+			return blogs, err
+		}
+		blogs = append(blogs, blog)
+	}
+	if err != nil {
+		return nil, err
+	}
+	return blogs, nil
+}
+
 func queryBlog(id int) (Article, error) {
 	blog := Article{}
-	err := Db.QueryRow("SELECT * FROM article WHERE id = $1", id).Scan(
+	err := db.QueryRow("SELECT * FROM article WHERE id = $1", id).Scan(
 		&blog.Id, &blog.Title, &blog.LastUpdate, &blog.Published,
 		&blog.ArticleType, &blog.Content, &blog.CreatedDate)
 	if err != nil {

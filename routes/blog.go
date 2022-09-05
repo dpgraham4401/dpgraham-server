cbcbefe0325a815513f638ea5af9edb8ba40daa4
David Paul Graham <dpgraham4401@gmail.com>
2022-09-05 09:09:18 -0400

    inline blog.go docs, replace IndentedJSON with JSON

--- routes/blog.go
+++ routes/blog.go
@@ -8,11 +8,12 @@ import (
 	"strings"
 )
 
+// GetBlog top level handlerFunc that returns a blog given an ID
 func GetBlog(c *gin.Context) {
 	id := c.Param("id")
 	idInt, err := strconv.Atoi(id)
 	if err != nil {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 	blogById, err := db.QueryBlog(idInt)
@@ -20,21 +21,25 @@ func GetBlog(c *gin.Context) {
 		c.JSON(http.StatusInternalServerError, nil)
 		return
 	} else {
-		c.IndentedJSON(http.StatusOK, blogById)
+		c.JSON(http.StatusOK, blogById)
 	}
 }
 
+// GetAllBlogs Top level handlerFunc that returns a list of Blog metadata
 func GetAllBlogs(c *gin.Context) {
 	allBlogs, err := db.QueryAllBlog()
 	if err != nil {
-		c.JSONP(http.StatusInternalServerError, "")
+		c.JSON(http.StatusInternalServerError, nil)
+		return
 	}
 	for i := 0; i < len(allBlogs); i++ {
 		allBlogs[i].Content = strings.ReplaceAll(allBlogs[i].Content, "#", "")
 		allBlogs[i].Content = strings.ReplaceAll(allBlogs[i].Content, "~", "")
+		// to show a preview, we slice the content down to the first 200 characters.
+		// This could be done, more efficiently, with the PSQL
 		if len(allBlogs[i].Content) >= 200 {
 			allBlogs[i].Content = allBlogs[i].Content[0:200]
 		}
 	}
-	c.IndentedJSON(http.StatusOK, allBlogs)
+	c.JSON(http.StatusOK, allBlogs)
 }

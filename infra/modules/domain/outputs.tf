b68b9cf4ff108bdcbd2e00dc98598702c7f7b234
David Paul Graham <43794491+dpgraham4401@users.noreply.github.com>
2023-08-14 14:36:44 -0400

    Relocate terraform (#38)
    * remove all old terraform from dpgraham-server
    
    all the infrastructure as code is being relocated to dpgraham-infra repository. We may add some repo specific terraform back in teh near future but for now, it's good to get rid of this
    
    * relocated workflows to dpgraham-infra

--- infra/modules/domain/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "dns_name" {
-  value = google_dns_managed_zone.dpgraham_com.dns_name
-}
-
-output "name" {
-  value = google_dns_managed_zone.dpgraham_com.name
-}
--- infra/modules/registry/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "id" {
-  value = google_artifact_registry_repository.gar.id
-}
-
-output "location" {
-  value = google_artifact_registry_repository.gar.location
-}
